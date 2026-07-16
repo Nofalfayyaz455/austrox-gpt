bde85858a62caa066428e952afcef33222f864e5
Nofal Fayyaz <nofalwarraich455@gmail.com>
2026-07-16 13:59:03 +0500

    Change current model to 'auto'

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ let currentUser = localStorage.getItem("currentUser");
 let messages = [];
 let currentChatId = Date.now();
 let currentMode = "quick";
-let currentModel = "google/gemma-3-27b-it";
+let currentModel = "auto";
 
 // ======================
 // Backend URL
